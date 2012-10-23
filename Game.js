d6305e1a7ca09930a34ba03a73e8b4d93ec15bcf
SwannLV <le.vigouroux.swann@gmail.com>
2012-10-23 01:37:37 -0400

    update

--- Game.js
+++ Game.js
@@ -5,8 +5,8 @@ var clock = new THREE.Clock();
 Physijs.scripts.worker = 'physijs_worker.js';
 Physijs.scripts.ammo = 'ammo.js';
 
-var camera, scene, renderer, composer, box;
-var sun_uniforms, sun_material, sun, Airship;
+var camera, scene, renderer, composer;
+var sun_uniforms, sun_material, sun, Airship, AirshipCamera;
 var spaceshipSpeed = 10.0;
 
 var keyboard  = new THREEx.KeyboardState();
@@ -14,28 +14,213 @@ var keyboard  = new THREEx.KeyboardState();
 var WIDTH = window.innerWidth || 2;
 var HEIGHT = window.innerHeight || 2;
 var FAR = 3500;
+var radius = 6371;
 
 init();
 animate();
 
+function initPlane () {
+    var plane = new THREE.Mesh( new THREE.PlaneGeometry( 10000, 10000 ), new THREE.MeshBasicMaterial( { color: 0x0000FF, opacity: 0.1, transparent: true } ) );
+    plane.position.y = -1;
+    plane.rotation.x = - Math.PI / 2;
+    scene.add( plane );
+}
+
+function initAirship () {
+  /*  var sphereGeom =  new THREE.SphereGeometry( 50, 32, 16 ); // radius, segmentsWidth, segmentsHeight
+    
+	
+	mirrorSphere = new THREE.Mesh( sphereGeom, mirrorSphereMaterial );
+	mirrorSphere.position.set(75,50,0);
+	scene.add(mirrorSphere);
+    */
+    var loader = new THREE.JSONLoader();
+    loader.load("spaceship.js", function (Geometry) {
+        /*Airship_mat  = Physijs.createMaterial(
+            new THREE.MeshPhongMaterial( { ambient: 0x555555, color: 0xFF0000, specular: 0xffffff, shininess: 500, shading: THREE.SmoothShading }),
+            .4, // low friction
+            .6 // high restitution
+        );
+        Airship = new Physijs.BoxMesh(
+        	new THREE.CubeGeometry( 50, 50, 50 ),
+    		cube_mat,
+            0
+    	);*/
+        AirshipCamera = new THREE.CubeCamera ( 0.1, 5000, 512 );//( 0.1, 5000, 512 );
+        // mirrorCubeCamera.renderTarget.minFilter = THREE.LinearMipMapLinearFilter;
+        scene.add( AirshipCamera );
+        var mirrorSphereMaterial = new THREE.MeshBasicMaterial( { envMap: AirshipCamera.renderTarget } );
+        //Geometry.materials[0].color.setHex(0xFFFFFF); // Sets the material color to red
+        Airship = new THREE.Mesh(Geometry, mirrorSphereMaterial);
+        //Airship = new THREE.Mesh(Geometry, new THREE.MeshFaceMaterial());
+        Airship.scale.set(2, 2, 2);
+        Airship.position.set (140, 170, 0);
+        AirshipCamera.position = Airship.position;
+        scene.add(Airship);
+    });
+}
+
+function initSun () {
+    sun_uniforms = {
+
+		fogDensity: { type: "f", value: 0.45 },
+		fogColor: { type: "v3", value: new THREE.Vector3( 0, 0, 0 ) },
+		time: { type: "f", value: 1.0 },
+		resolution: { type: "v2", value: new THREE.Vector2() },
+		uvScale: { type: "v2", value: new THREE.Vector2( 3.0, 1.0 ) },
+		texture1: { type: "t", value: THREE.ImageUtils.loadTexture( "textures/cloud.png" ) },
+		texture2: { type: "t", value: THREE.ImageUtils.loadTexture( "textures/lavatext.jpg" ) }
+
+	};
+
+	sun_uniforms.texture1.value.wrapS = sun_uniforms.texture1.value.wrapT = THREE.RepeatWrapping;
+	sun_uniforms.texture2.value.wrapS = sun_uniforms.texture2.value.wrapT = THREE.RepeatWrapping;
+
+	sun_material = new THREE.ShaderMaterial( {
+
+		uniforms: sun_uniforms,
+		vertexShader: document.getElementById( 'vertexShader' ).textContent,
+
+		fragmentShader: document.getElementById( 'fragmentShader' ).textContent
+	} );
+
+	sun = new THREE.Mesh( new THREE.SphereGeometry( 600, 200, 30, 30 ), sun_material );
+	sun.rotation.z = 6*3.14/4;
+    sun.position.x = 0;
+    sun.position.y = 700;
+    sun.position.z = -3000;
+	scene.add( sun );
+}
+
+function initGrid () {
+    //Shorten the vertex function
+    function v(x,y,z){
+        return new THREE.Vertex(new THREE.Vector3(x,y,z));
+    }
+
+    //Create line (point1, point2, colour)
+    function createLine(p1, p2, color){
+        var line, lineGeometry = new THREE.Geometry(),
+        lineMat = new THREE.LineBasicMaterial({color: color, lineWidth: 1});
+        lineGeometry.vertices.push(p1, p2);
+        line = new THREE.Line(lineGeometry, lineMat);
+        scene.add(line);
+    }
+
+   // Grid creation
+   var squareLength = 100;
+   var gridXNumber = 3;
+   var gridZNumber = FAR / squareLength;
+   for (var i = - gridXNumber; i <= gridXNumber; i++){
+        createLine(v(i/2 * squareLength, 0, FAR), v(i/2 * squareLength, 0, -FAR), 0xFFFFFF);
+   }
+   for (var j = - gridZNumber; j <= gridZNumber; j++){
+        createLine(v(gridXNumber * squareLength / 2, 0, j*100), v(-gridXNumber * squareLength / 2, 0, j*100), 0xFFFFFF);
+   }
+}
+
+function initStars (lineX, lineY, lineZ, scale) {
+
+    var i, line, vertex1, vertex2, material, p,
+		parameters = [ [ 0.25, 0xff7700, 1, 2 ], [ 0.5, 0xff9900, 1, 1 ], [ 0.75, 0xffaa00, 0.75, 1 ], [ 1, 0xffaa00, 0.5, 1 ], [ 1.25, 0x000833, 0.8, 1 ],
+				       [ 3.0, 0xaaaaaa, 0.75, 2 ], [ 3.5, 0xffffff, 0.5, 1 ], [ 4.5, 0xffffff, 0.25, 1 ], [ 5.5, 0xffffff, 0.125, 1 ] ],
+
+		geometry = new THREE.Geometry();
+
+
+	for ( i = 0; i < 1500; i ++ ) {
+
+		var vertex1 = new THREE.Vector3();
+		vertex1.x = Math.random() * 2 - 1;
+		vertex1.y = Math.random() * 2 - 1;
+		vertex1.z = Math.random() * 2 - 1;
+		vertex1.normalize();
+		vertex1.multiplyScalar( 450 );
+
+		vertex2 = vertex1.clone();
+		vertex2.multiplyScalar( Math.random() * 0.09 + 1 );
+
+		geometry.vertices.push( vertex1 );
+		geometry.vertices.push( vertex2 );
+
+	}
+
+	for( i = 0; i < parameters.length; ++ i ) {
+
+		p = parameters[ i ];
+
+		material = new THREE.LineBasicMaterial( { color: p[ 1 ], opacity: p[ 2 ], linewidth: p[ 3 ] } );
+
+		line = new THREE.Line( geometry, material, THREE.LinePieces );
+		line.scale.x = line.scale.y = line.scale.z = p[ 0 ] * scale;
+		line.originalScale = p[ 0 ];
+		line.rotation.y = Math.random() * Math.PI;
+        line.position.x = lineX;
+        line.position.y = lineY;
+        line.position.z = lineZ;
+		line.updateMatrix();
+		scene.add( line );
+
+	}
+}
+
+// INIT
 function init() {
 
 	container = document.getElementById( 'container' );
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    container.appendChild( renderer.domElement );
+	renderer.autoClear = true;
 
+    // CAMERA
 	camera = new THREE.PerspectiveCamera( 45, WIDTH / HEIGHT, 1, FAR );
     camera.position.z = 400;
     camera.position.y = 200;
 
 	scene = new Physijs.Scene;
 
-    // Plane
-    var plane = new THREE.Mesh( new THREE.PlaneGeometry( 10000, 10000 ), new THREE.MeshBasicMaterial( { color: 0x0000FF, opacity: 0.1, transparent: true } ) );
-    plane.position.y = -1;
-    plane.rotation.x = - Math.PI / 2;
-    scene.add( plane );
+    // LIGHTS
+    var dirLight = new THREE.DirectionalLight( 0xffffff, 0.125 );
+    dirLight.position.set( 0, 0, 1 ).normalize();
+    scene.add( dirLight );
+    var pointLight = new THREE.PointLight( 0xffffff, 1.5 );
+    pointLight.position.set( 0, 100, -3500 );
+    scene.add( pointLight );
+    scene.fog = new THREE.Fog( 0x000000, 250, 3000 );
+    
+    // OBJECTS
+    initStars(0, 100, -10000, 0.5);
+    initStars(0, 300, -5000, 10);
+    initPlane();
+    initAirship();
+    initSun();
+    initGrid();
+    
+    // STATS
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+	stats.domElement.style.top = '20px';
+    stats.domElement.style.left = '20px';
+	container.appendChild( stats.domElement );
 
-	// Materials
-	/*var ground_material = Physijs.createMaterial(
+    // EFFECTS
+	var renderModel = new THREE.RenderPass( scene, camera );
+	//var effectBloom = new THREE.BloomPass( 3.25 );
+	var effectFilm = new THREE.FilmPass( 0.35, 0.95, 2048, false );
+
+	effectFilm.renderToScreen = true;
+	composer = new THREE.EffectComposer( renderer );
+	composer.addPass( renderModel );
+	//composer.addPass( effectBloom );
+	composer.addPass( effectFilm );
+
+    // RESIZE
+	onWindowResize();
+	window.addEventListener( 'resize', onWindowResize, false );
+}
+
+// Materials
+    /*var ground_material = Physijs.createMaterial(
 		new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture( 'textures/lavatext.jpg' ) }),
 		.8, // high friction
 		.4 // low restitution
@@ -66,30 +251,6 @@ function init() {
 		.6 // high restitution
 	);*/
 
-     // Airship
-    var loader = new THREE.JSONLoader();
-    loader.load("spaceship.js", function (Geometry) {
-        /*Airship_mat  = Physijs.createMaterial(
-            new THREE.MeshPhongMaterial( { ambient: 0x555555, color: 0xFF0000, specular: 0xffffff, shininess: 500, shading: THREE.SmoothShading }),
-            .4, // low friction
-        	.6 // high restitution
-        );
-        Airship = new Physijs.BoxMesh(
-        	new THREE.CubeGeometry( 50, 50, 50 ),
-    		cube_mat,
-            0
-    	);*/
-        //Geometry.materials[0].color.setHex(0xFFFFFF); // Sets the material color to red
-        Airship = new THREE.Mesh(Geometry, new THREE.MeshFaceMaterial());
-        Airship.scale.set(2, 2, 2);
-        Airship.position.set(20, 0, -100); // Positions the mesh right from the center
-        Airship.position.y   = 235;
-        Airship.position.x   = 0;
-        Airship.position.z   = 0;
-        //Airship.rotation.y   = - Math.PI / 2;
-        scene.add(Airship);
-    });
-
     // Box
     /*box = new Physijs.BoxMesh(
 		new THREE.CubeGeometry( 100, 100, 100 ),
@@ -109,116 +270,6 @@ function init() {
 	//box.castShadow = true;
 	scene.add( box );*/
 
-    // LIGHTS
-    var dirLight = new THREE.DirectionalLight( 0xffffff, 0.125 );
-    dirLight.position.set( 0, 0, 1 ).normalize();
-    scene.add( dirLight );
-    var pointLight = new THREE.PointLight( 0xffffff, 1.5 );
-    pointLight.position.set( 0, 100, -3500 );
-    scene.add( pointLight );
-
-    // Fog
-    scene.fog = new THREE.Fog( 0x000000, 250, 3000 );
-
-    // Sun
-	sun_uniforms = {
-
-		fogDensity: { type: "f", value: 0.45 },
-		fogColor: { type: "v3", value: new THREE.Vector3( 0, 0, 0 ) },
-		time: { type: "f", value: 1.0 },
-		resolution: { type: "v2", value: new THREE.Vector2() },
-		uvScale: { type: "v2", value: new THREE.Vector2( 3.0, 1.0 ) },
-		texture1: { type: "t", value: THREE.ImageUtils.loadTexture( "textures/cloud.png" ) },
-		texture2: { type: "t", value: THREE.ImageUtils.loadTexture( "textures/lavatext.jpg" ) }
-
-	};
-
-	sun_uniforms.texture1.value.wrapS = sun_uniforms.texture1.value.wrapT = THREE.RepeatWrapping;
-	sun_uniforms.texture2.value.wrapS = sun_uniforms.texture2.value.wrapT = THREE.RepeatWrapping;
-
-	sun_material = new THREE.ShaderMaterial( {
-
-		uniforms: sun_uniforms,
-		vertexShader: document.getElementById( 'vertexShader' ).textContent,
-
-		fragmentShader: document.getElementById( 'fragmentShader' ).textContent
-	} );
-
-	sun = new THREE.Mesh( new THREE.SphereGeometry( 600, 200, 30, 30 ), sun_material );
-	sun.rotation.z = 6*3.14/4;
-    sun.position.x = 0;
-    sun.position.y = 700;
-    sun.position.z = -3000;
-	scene.add( sun );
-
-    //
-
-    //Shorten the vertex function
-    function v(x,y,z){
-        return new THREE.Vertex(new THREE.Vector3(x,y,z));
-    }
-
-    //Create line (point1, point2, colour)
-    function createLine(p1, p2, color){
-        var line, lineGeometry = new THREE.Geometry(),
-        lineMat = new THREE.LineBasicMaterial({color: color, lineWidth: 1});
-        lineGeometry.vertices.push(p1, p2);
-        line = new THREE.Line(lineGeometry, lineMat);
-        scene.add(line);
-    }
-
-   // Grid creation
-   var squareLength = 100;
-   var gridXNumber = 3;
-   var gridZNumber = FAR / squareLength;
-   for (var i = - gridXNumber; i <= gridXNumber; i++){
-        createLine(v(i/2 * squareLength, 0, FAR), v(i/2 * squareLength, 0, -FAR), 0xFFFFFF);
-   }
-   for (var j = - gridZNumber; j <= gridZNumber; j++){
-        createLine(v(gridXNumber * squareLength / 2, 0, j*100), v(-gridXNumber * squareLength / 2, 0, j*100), 0xFFFFFF);
-   }
-    /*for (var i = - WIDTH/100; i <= WIDTH/100 + 1; i++){
-        createLine(v(i/2 * 100, 0, FAR), v(i/2 * 100, 0, -FAR), 0xFFFFFF);
-   }
-   for (var j = - FAR/100; j <= FAR/100; j++){
-        createLine(v(WIDTH/2, 0, j*100), v(-WIDTH/2, 0, j*100), 0xFFFFFF);
-   }*/
-
-    //
-
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	container.appendChild( renderer.domElement );
-	renderer.autoClear = false;
-
-	//
-
-	stats = new Stats();
-	stats.domElement.style.position = 'absolute';
-	stats.domElement.style.top = '20px';
-    stats.domElement.style.left = '20px';
-	container.appendChild( stats.domElement );
-
-	//
-
-	var renderModel = new THREE.RenderPass( scene, camera );
-	//var effectBloom = new THREE.BloomPass( 3.25 );
-	var effectFilm = new THREE.FilmPass( 0.35, 0.95, 2048, false );
-
-	effectFilm.renderToScreen = true;
-
-	composer = new THREE.EffectComposer( renderer );
-
-	composer.addPass( renderModel );
-	//composer.addPass( effectBloom );
-	composer.addPass( effectFilm );
-
-	//
-
-	onWindowResize();
-
-	window.addEventListener( 'resize', onWindowResize, false );
-
-}
 
 function onWindowResize( event ) {
 
@@ -234,55 +285,20 @@ function onWindowResize( event ) {
 
 }
 
-//
-/*
-function AddMesh( geometry ) {
-
-    //geometry.materials[0][0].shading = THREE.FlatShading;
-	//geometry.materials[0][0].morphTargets = true;
-alert('ok');
-	var material = new THREE.MeshFaceMaterial();
-alert('ok');
-	Airship = new THREE.Mesh( geometry, material );
-	Airship.scale.set(50, 50, 50);
-    Airship.position.x = 20;
-    Airship.position.z = -50;
-
-	scene.addObject( Airship );
-}*/
-
-//
-
 function animateAirship()
 {
      if( keyboard.pressed("left") ) {
         Airship.position.x -= 5;
-        //speedRotZ = 0.05;
     }
     else if( keyboard.pressed("right") ) {
         Airship.position.x += 5;
-        //speedRotZ = -0.05;
     }
-   /* else if (speedRotZ != 0)
-    {
-        speedRotZ = 0.00;
-    }*/
     if( keyboard.pressed("up") ) {
         Airship.position.y += 5;
     }
     else if( keyboard.pressed("down") && Airship.position.y > 35) {
         Airship.position.y -= 5;
     }
-    /*if( keyboard.pressed("space") ) {
-        //cube.position.z += 1;
-        Airship.rotation.x -= speedRotX / 2;
-    }
-    else{
-        //cube.position.z -= 2;
-        Airship.rotation.x -= speedRotX;
-    }*/
-    //camera.position.z -= 1;
-    //cube.rotation.z += speedRotZ;
 }
 
 function animateGrid()
@@ -290,8 +306,13 @@ function animateGrid()
     for ( var i = 0; i < scene.children.length; i ++ ) {
         var object = scene.children[ i ];
         if ( object instanceof THREE.Line ) {
-            if (object.position.z >= 100*10){
+            if (object.position.y == 0 && object.position.z >= 1000){
                 object.position.z = 0;
+            }
+            else if (object.position.z >= 5000){
+                object.position.z = -5000;
+                /*object.position.x = Math.random() * 2 - 1;
+		        object.position.y = Math.random() * 2 - 1;*/
             }
             object.position.z += spaceshipSpeed;
         }
@@ -301,7 +322,6 @@ function animateGrid()
 function animate() {
 
 	requestAnimationFrame( animate );
-    //box.position.z += spaceshipSpeed;
     animateAirship();
     animateGrid();
 	render();
@@ -313,11 +333,20 @@ function render() {
 
 	var delta = 5 * clock.getDelta();
 
-    camera.lookAt( Airship.position );
-    //scene.simulate(undefined, 2);
+    if (Airship) {
+        camera.lookAt( Airship.position );
+    }
 	sun_uniforms.time.value += 0.2 * delta;
 
-	renderer.clear();
-	composer.render( 0.01 );
+	
+    
+    Airship.visible = false;
+    AirshipCamera.updateCubeMap( renderer, scene );
+	Airship.visible = true;
+	
+	renderer.render( scene, camera );
+    
+    renderer.clear();
+    composer.render( 0.01 );
 
 }
